2248f9b680f75c32218acaa14262c9e10396e364
Maquete Virtual <maquetevirtual.ufba@gmail.com>
2016-09-20 15:46:59 -0300

    (CBR) Capsula caminhante
    pathfinding e ajustes terreno

---
+++ Assets/SampleAgentScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SampleAgentScript : MonoBehaviour {
+
+	public Transform target;
+	NavMeshAgent agent;
+
+	void Start () {
+		agent = GetComponent<NavMeshAgent> ();
+	}
+	
+
+	void Update () {
+		agent.SetDestination (target.position);//makes the object move to the target's position
+	}
+}
