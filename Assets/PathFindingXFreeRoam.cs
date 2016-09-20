c0fca303479633028c53c491aebb03dd49f9f278
Maquete Virtual <maquetevirtual.ufba@gmail.com>
2016-09-20 16:59:00 -0300

    ()CBR

---
+++ Assets/PathFindingXFreeRoam.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PathFindingXFreeRoam : MonoBehaviour {
+	CharacterController contro = new CharacterController();
+	NavMesh nav = new NavMesh();
+	SampleAgentScript script = new SampleAgentScript();
+	GameObject s = new GameObject ();
+	bool ativado = false;
+	void Start () {
+	
+	}
+	
+
+	void Update () {
+		if(Event.current.keyCode == KeyCode.P){
+			
+			//a.SetActive (true);
+		//	b.SetActive (false);
+			if (ativado == true) {
+				ativado = false;
+			} else {
+				ativado = true;
+			}
+	}
+
+}
+}
