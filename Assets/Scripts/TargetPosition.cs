29622ef1d675a372a30cde2e5545ec621d9c44ae
Maquete Virtual <maquetevirtual.ufba@gmail.com>
2016-10-04 16:10:26 -0300

    (CBR) AutoNavigation
    Troca camera funciona. No modo FPS, ao clicar em algum lugar do mapa, e
    apertar a tecla "I", o jogador irá se deslocar automaticamente para lá

---
+++ Assets/Scripts/TargetPosition.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+public class TargetPosition : MonoBehaviour
+{
+	Vector3 newPosition;
+
+	void Start () {
+		newPosition = transform.position;
+	}
+	void Update()
+	{
+		if (Input.GetMouseButtonDown(0))
+		{
+			RaycastHit hit;
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			if (Physics.Raycast(ray, out hit))
+			{
+				newPosition = hit.point;
+				transform.position = newPosition;
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/TrocaCamera.js
@@ -0,0 +1,20 @@
+﻿#pragma strict
+
+var cam1 : Camera ;
+var cam2 : Camera;
+
+function Start() {
+    cam1.enabled = true;
+    cam2.enabled = false;
+}
+ 
+function Update() {
+ 
+    if (Input.GetKeyUp (KeyCode.C)) {
+        
+        cam1.enabled = !cam1.enabled;
+        cam2.enabled = !cam2.enabled;
+       //transform.position = new Vector3(2708.997, -584.0, -3398.0);
+        
+    }
+}
