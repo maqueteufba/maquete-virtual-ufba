0667b6013d023766e26574c61a193e521cafcf97
cainasurf <cainabrazil@gmail.com>
2016-10-19 16:59:29 -0300

    (CBR) atualiza script troca_camera + box colliders

---
+++ Assets/Scripts/TrocaCamera.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrocaCamera : MonoBehaviour {
+	public Camera cam1, cam2;
+
+	// Use this for initialization
+	void Start () {
+		cam1.enabled = true;
+		cam2.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyUp (KeyCode.C)) {
+
+			cam1.enabled = !cam1.enabled;
+
+			//liga ou desliga o cursor
+			if(cam1.enabled == false)
+				Cursor.visible = true;
+			else
+				Cursor.visible = false;
+
+			cam2.enabled = !cam2.enabled;
+			//transform.position = new Vector3(2708.997, -584.0, -3398.0);
+
+		}
+	}
+}
